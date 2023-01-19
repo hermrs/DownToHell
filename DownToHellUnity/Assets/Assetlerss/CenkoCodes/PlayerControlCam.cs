fab16040a9ad58b73f55894446fd8d63495083a2
Üzeyir Cenker Arda <202104115040@dogus.edu.tr>
2023-01-20 00:44:24 +0300

    Kayma Zıplama Hareket Ve Bilimum Kontrol Dinamikleri
    Kayma Zıplama Hareket Ve Bilimum Kontrol Dinamikleri
    Pov Kamera takibi

---
+++ DownToHellUnity/Assets/Assetlerss/CenkoCodes/PlayerControlCam.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class PlayerControlCam : MonoBehaviour
+{
+    public float senX;
+    public float senY;
+    public Transform dönme;
+    float xDönme;
+    float yDönme;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * senX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * senY;
+        yDönme += mouseX;
+        xDönme -= mouseY;
+        xDönme = Mathf.Clamp(xDönme, -90f, 90f);
+        // kameryý döndürme &6
+        transform.rotation = Quaternion.Euler(xDönme, yDönme, 0);
+        dönme.rotation = Quaternion.Euler(0, yDönme, 0);
+       
+    } 
+}
